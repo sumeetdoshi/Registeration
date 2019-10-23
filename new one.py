255f74da77c3757b0f4d15fa1a415fe521f7525a
sumeetdoshi <56690156+sumeetdoshi@users.noreply.github.com>
2019-10-23 20:40:39 +0530

    Delete new one.py

--- new one.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-root = Tk()
-root.geometry("733x433")
-root.minsize(200,100)
-root.maxsize(500,500)
-sumeet = Label(text="PYCHARM COMMUNITY EDITION",pady=100)
-sumeet.pack()
-root.mainloop()
-
